00682fc2db127c85d16c869ba81752f0a8bdbb55
Maarten van Iterson <M.van_iterson.HG@lumc.nl>
2015-01-02 10:35:45 +0000

    Updating citation info
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/MethylAid@97958 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/MethylAid/global.R
@@ -0,0 +1,4 @@
+##global accessible by all users
+if(!require(MethylAid))
+  stop("MethylAid should be installed!")
+data(exampleData, package="MethylAid")
--- inst/MethylAid/server.R
+++ inst/MethylAid/server.R
@@ -1,7 +1,2 @@
 ##demo version of the MethylAid App
-
-##global accessible by all users
-library(MethylAid)
-data(exampleData, package="MethylAid")
-
 MethylAid:::server450k(exampleData)
